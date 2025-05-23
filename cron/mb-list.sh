5d9a0e7121def9842a8864a4143744337b3d382c
Takeshi Ara <ara@nig.ac.jp>
2025-05-23 15:49:26 +0900

    update for phase4

--- cron/mb-list.sh
+++ cron/mb-list.sh
@@ -2,6 +2,6 @@
 
 # generate mb study livelist and filelist daily
 cd /home/mbadmin
-/opt/pkg/singularity-ce/4.0.0/bin/singularity exec /home/mbadmin/mb-tools.simg mb-livelist
-/opt/pkg/singularity-ce/4.0.0/bin/singularity exec -B /lustre9/open/shared_data/metabobank/study:/lustre9/open/shared_data/metabobank/study /home/mbadmin/mb-tools.simg mb-filelist
+/opt/pkg/singularity-ce/4.2.1/bin/singularity exec /home/mbadmin/mb-tools.simg mb-livelist
+/opt/pkg/singularity-ce/4.2.1/bin/singularity exec -B /lustre9/open/shared_data/metabobank/study:/lustre9/open/shared_data/metabobank/study /home/mbadmin/mb-tools.simg mb-filelist
 
